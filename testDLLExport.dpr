81c81a840f83c906c09c9ad70fb98e6085710c3b
marius maximus <mariusz.maximus@gmail.com>
2017-11-19 19:56:49 +0100

    test callback

---
+++ testDLLExport.dpr
@@ -0,0 +1,49 @@
+program testDLLExport;
+
+uses Windows, sysutils;
+
+const
+  DLL_PATH= '.\testDLLExport\bin\Debug\x86\testDLLExport.dll';
+
+procedure TestReturnString(var stringDelphi: WideString); cdecl; external DLL_PATH;
+
+
+
+type
+  TProcCallback = procedure (p_int: integer);  cdecl;
+
+//function DoSendCallbackFunction(ptr:Pointer): integer; cdecl; external 'DraminskiComLibrary.dll';
+function DoSendCallbackFunction(ptr:TProcCallback): integer; cdecl; external DLL_PATH;
+
+
+var
+  str: WideString;
+
+
+procedure TestCallbacFunction(p_int: integer); cdecl;
+begin
+   Writeln('p_int = ',p_int);
+end;
+
+
+procedure TestCallback;
+begin
+  DoSendCallbackFunction(TestCallbacFunction);
+end;
+
+
+
+begin
+  Allocconsole;
+  Writeln('DELPHI->BEGIN');
+
+  Writeln('DELPHI->Return string from c#');
+  TestReturnString(str);
+  Writeln('DELPHI->Str=',str);
+
+  DoSendCallbackFunction(TestCallbacFunction);
+
+  TestCallback;
+  Writeln('DELPHI->END');
+  readln;
+end.
---
+++ testDLLExport/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace testDLLExport
+{
+    public static class Class1
+    {
+
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
+        public static void TestReturnString([MarshalAs(UnmanagedType.BStr)]  ref string s_out)
+        {
+            s_out = "Hello from .Net !";// "Hello from .Net " + s;
+        }
+
+
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void TProcCallback(int dane);
+
+        [DllExport("DoSendCallbackFunction", CallingConvention = CallingConvention.StdCall)]
+        public static int DoSendCallbackFunction(IntPtr callback)
+        {
+            TProcCallback myCallback = (TProcCallback)Marshal.GetDelegateForFunctionPointer(callback, typeof(TProcCallback));
+            int testInt = 123;
+            Console.WriteLine("C#->Before Callback");
+            myCallback(testInt);
+            Console.WriteLine("C#->After Callback");
+            return 701;
+        }
+
+
+    }
+}
